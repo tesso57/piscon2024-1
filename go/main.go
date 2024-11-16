7352105bca1c6698ce56275af1a846162d32820a
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-17 02:17:05 +0900

    count

--- go/main.go
+++ go/main.go
@@ -1302,7 +1302,7 @@ func postIsuCondition(c echo.Context) error {
 	// defer tx.Rollback()
 
 	var count int
-	err = db.Get(&count, "SELECT COUNT(*) FROM `isu` WHERE `jia_isu_uuid` = ?", jiaIsuUUID)
+	err = db.Get(&count, "SELECT COUNT(*) FROM `isu` WHERE `jia_isu_uuid` = ? LIMIT 1", jiaIsuUUID)
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
 		return c.NoContent(http.StatusInternalServerError)
