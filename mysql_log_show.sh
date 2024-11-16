d2a511c0bbdd02424b271287cfef477f88c38555
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-17 02:50:31 +0900

    less

--- mysql_log_show.sh
+++ mysql_log_show.sh
@@ -1,1 +1,1 @@
-cd ~/log && cat $(ls -t mysql-slow.log-* | head -n 1)
+cd ~/log && less $(ls -t mysql-slow.log-* | head -n 1)
--- nginx_log_show.sh
+++ nginx_log_show.sh
@@ -1,1 +1,1 @@
-cd ~/log && cat $(ls -t access.log-* | head -n 1)
+cd ~/log && less $(ls -t access.log-* | head -n 1)
