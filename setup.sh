cceed4640915e9c639e1af62294500690179d182
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-16 15:44:24 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ DB_PASS=isucon
 DB_PORT=3306
 DB_HOST=localhost
 
-mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -P$DB_PORT "set global slow_query_log_file = \"/tmp/slow.log\"; set global long_query_time = 0; set global slow_query_log = ON;"
+mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -P$DB_PORT -e "set global slow_query_log_file = \"/tmp/slow.log\"; set global long_query_time = 0; set global slow_query_log = ON;"
 sudo systemctl restart mysql
 sudo systemctl restart isucondition.go
 sudo systemctl restart nginx
