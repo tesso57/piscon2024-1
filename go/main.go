eaf9bf92662fe24cedfd01606381b31b123e3a96
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-21 02:55:34 +0900

    uds

--- go/main.go
+++ go/main.go
@@ -399,7 +399,7 @@ func main() {
 	//
 	// e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	// e.JSONSerializer = &JSONSerializer{}
+	e.JSONSerializer = &JSONSerializer{}
 	e.POST("/initialize", postInitialize)
 
 	e.Use(
@@ -446,15 +446,15 @@ func main() {
 
 	if os.Getenv("SRVNO") == "1" {
 		go insertIsuConditionScheduled(time.Millisecond * 100)
-		// listener, isUnixDomainSock, err := newUnixDomainSockListener()
-		// if err != nil {
-		// 	e.Logger.Fatalf("failed to create unix domain socket listener: %v", err)
-		// 	return
-		// }
-		//
-		// if isUnixDomainSock {
-		// 	e.Listener = listener
-		// }
+		listener, isUnixDomainSock, err := newUnixDomainSockListener()
+		if err != nil {
+			e.Logger.Fatalf("failed to create unix domain socket listener: %v", err)
+			return
+		}
+
+		if isUnixDomainSock {
+			e.Listener = listener
+		}
 		// } else {
 		go calculateTrendScheduled(time.Millisecond * 100)
 	}
