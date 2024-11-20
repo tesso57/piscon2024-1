7c0b884633ecc52c4be6537a743c7c70a6f7d7d9
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-20 21:36:58 +0900

    go

--- s3.sh
+++ s3.sh
@@ -4,5 +4,5 @@ sudo sysctl -p
 sudo systemctl stop nginx
 sudo systemctl stop mysql
 cd go
-go build -o isucondition -gcflags='-l=4'
+/home/isucon/local/go/bin/go build -o isucondition -gcflags='-l=4'
 sudo systemctl restart isucondition.go.service
