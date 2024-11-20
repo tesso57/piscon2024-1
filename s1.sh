5912338fbe39c86e2ab79fca7b5a9b800319b15d
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-20 22:15:36 +0900

    delete gcflag

--- s1.sh
+++ s1.sh
@@ -5,5 +5,5 @@ sudo sysctl -p
 sudo systemctl restart nginx
 sudo systemctl stop mysql
 cd go
-/home/isucon/local/go/bin/go build -o isucondition -gcflags='-l=4'
+/home/isucon/local/go/bin/go build -o isucondition 
 sudo systemctl restart isucondition.go.service
