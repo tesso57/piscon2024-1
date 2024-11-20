0e49868b6da4d1732876856728e6669f65010976
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-20 21:33:35 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-ssh piscon23-1-1 "cd ./webapp; git pull; ./s1.sh"
-ssh piscon23-1-2 "cd ./webapp; git pull; ./s2.sh"
-ssh piscon23-1-3 "cd ./webapp; git pull; ./s3.sh"
+ssh piscon24-1-1 "cd ./webapp; git pull; ./s1.sh"
+ssh piscon24-1-2 "cd ./webapp; git pull; ./s2.sh"
+ssh piscon24-1-3 "cd ./webapp; git pull; ./s3.sh"
