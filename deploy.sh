c9becc2857bbc47fa7f3711e74fc7deb3cf444a0
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-20 21:32:14 +0900

    git pu

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-ssh piscon23-1-1 "cd ./webapp; ./s1.sh"
-ssh piscon23-1-2 "cd ./webapp; ./s2.sh"
-ssh piscon23-1-3 "cd ./webapp; ./s3.sh"
+ssh piscon23-1-1 "cd ./webapp; git pull; ./s1.sh"
+ssh piscon23-1-2 "cd ./webapp; git pull; ./s2.sh"
+ssh piscon23-1-3 "cd ./webapp; git pull; ./s3.sh"
