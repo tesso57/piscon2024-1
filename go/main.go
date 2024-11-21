725bc22486c8a5a656eeec0fe43392b5cee225de
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-21 22:32:31 +0900

    max con

--- go/main.go
+++ go/main.go
@@ -476,8 +476,8 @@ func main() {
 		e.Logger.Fatalf("failed to connect db: %v", err)
 		return
 	}
-	db.SetMaxOpenConns(512)
-	db.SetMaxIdleConns(512)
+	db.SetMaxOpenConns(1024)
+	db.SetMaxIdleConns(1024)
 	defer db.Close()
 
 	postIsuConditionTargetBaseURL = os.Getenv("POST_ISUCONDITION_TARGET_BASE_URL")
