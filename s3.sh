43ca14cc4a824a3fff7e12f7a1db74d2947d0346
tesso <48358052+tesso57@users.noreply.github.com>
2024-11-21 21:36:29 +0900

    disable S3

--- s3.sh
+++ s3.sh
@@ -3,6 +3,8 @@ sudo cp ./sysctl.conf /etc/sysctl.conf
 sudo sysctl -p
 sudo systemctl stop nginx
 sudo systemctl stop mysql
-cd go
-/home/isucon/local/go/bin/go build -o isucondition
-sudo systemctl restart isucondition.go.service
+sudo systemctl stop isucondition.go.service
+# cd go
+#
+# /home/isucon/local/go/bin/go build -o isucondition
+# sudo systemctl restart isucondition.go.service
